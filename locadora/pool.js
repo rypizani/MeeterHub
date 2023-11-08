71b358393fa0b5a91f518f2311563189e896fdc0
Marcelo Miguel Ferreira <84048133+Lion9m@users.noreply.github.com>
2023-11-08 15:31:55 -0300

    pool criado

---
+++ locadora/pool.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2')
+
+const pool = mysql.createPool(
+   { 
+    host: '31.170.160.103',
+    user: 'u226780189_Meeter',
+    password:  'Meeterhub2023',
+    database: 'u226780189_Meeter'
+
+});
+
+module.exports = {
+    pool,
+}
