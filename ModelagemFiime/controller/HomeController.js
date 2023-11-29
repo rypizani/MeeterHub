ab3d03ed6e21fe6dff1967ab4cf46193cea77fc5
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2023-11-29 16:42:08 -0300

    Update
    29.11 - 16:41
    
    Feito createToken para o registro de novos usuários.

--- ModelagemFiime/controller/HomeController.js
+++ ModelagemFiime/controller/HomeController.js
@@ -1,7 +1,20 @@
 const bcrypt = require('bcrypt');
 const { Registro } = require('../models/Registro/Registro');
 const { RegistroController } = require('../controller/Registro/RegistroController');
+// const transporter = require('./mailer');
+// const nodemailer = require('nodemailer');
+const { JWTController } = require('../middleware/JWTController');
 
+const transporter = nodemailer.createTransport({
+    
+    host: "smtp.hostinger.com", // Substitua "yourdomain.com" pelo seu domínio na Hostinger
+    port: 465, // A porta pode ser 587 ou 465, dependendo das configurações do seu provedor
+    secure: true, // Use true para a porta 465, false para a porta 587
+    auth: {
+     user: "contato@rhuna.tech", // Substitua pelo seu endereço de e-mail
+     pass: "Rhunatech2023#", // Substitua pela sua senha
+    }
+})
 
 exports.HomeController = {
 
@@ -23,47 +36,25 @@ exports.HomeController = {
         });
 
         //Crete token para armazenar o token de acesso
+        const token = JWTController.createToken({ email: registro.email }, true);
 
-        res.json({
-            message: 'Cadastro realizado com sucesso', registro: {
-                nome: registro.nome, 
-                email: registro.email, 
-                endereco: registro.endereco, 
-                CEP: registro.CEP
-            }
-        })
+        const responseData = {
+            user: {
+              nome: registro.nome,
+              emailprinc: registro.emailprinc,
+              tpusuario: registro.tpusuario,
+              cpf: registro.cpf,
+            },
+          };
 
-
+          res.json({responseData, accessToken: token.access_token});
 
     },
 
-    async  login(req, res, next) { // ou req.body, dependendo de como você está enviando os dados
+    async login(req, res, next) { // ou req.body, dependendo de como você está enviando os dados
 
         const registro = await RegistroController.get
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    
         // try {
         //     const registro = await RegistroController.getByEmail;
             
@@ -89,6 +80,27 @@ exports.HomeController = {
         //     console.error(error);
         //     res.status(500).json({ mensagem: 'Erro durante a busca e comparação de senha' });
         // }
-    }
+    },
+
 }
+	
+// const  transport = nodemailer.createTransport({smtpOptions});
+ 
+//    let  message = `<p>Use esse token para resetar sua senha <code>/apiRouter/reset-password</code> api route:</p>
+//                        <p><code>${resetToken}</code></p>`; // Here you can replace the message with your HTML code.
+ 
+//   const  mailOptions = {
+//     from: process.env.EMAIL_FROM,
+//     to: email, // the user email
+//     subject: ' Reset your Password',
+//     html: `<h4>Reset Password</h4>
+//                    ${message}`
+//    };
+ 
+//    const  info = transport.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//             return console.log(error);
+//         }
+//         console.log("Message sent: %s", info.messageId);
+// });
 
---
+++ ModelagemFiime/middleware/JWTController.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+exports.JWTController = {
+
+    createToken(payload, refresh = false) {
+        const accessToken = jwt.sign(payload, process.env.SECRET, {
+          expiresIn: "1h",
+        });
+        return {
+          access_token: accessToken,
+          refresh_token: refresh
+            ? jwt.sign(payload,process.env.SECRET, {
+                expiresIn:"1h",
+              })
+            : null,
+        };
+    },
+
+}
