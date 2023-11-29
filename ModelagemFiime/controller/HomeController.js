48dd7dfaf8a0af408cc8a962039b2a9661953ee4
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2023-11-29 17:20:44 -0300

    Update

--- ModelagemFiime/controller/HomeController.js
+++ ModelagemFiime/controller/HomeController.js
@@ -1,34 +1,33 @@
 const bcrypt = require('bcrypt');
 const { Registro } = require('../models/Registro/Registro');
 const { RegistroController } = require('../controller/Registro/RegistroController');
-// const transporter = require('./mailer');
-// const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 const { JWTController } = require('../middleware/JWTController');
 
-// const transporter = nodemailer.createTransport({
-//     host: "smtp.titan.email", // Substitua "yourdomain.com" pelo seu domínio na Hostinger
-//     port: 465, // A porta pode ser 587 ou 465, dependendo das configurações do seu provedor
-//     secure: true, // Use true para a porta 465, false para a porta 587
-//     auth: {
-//       user: "contato@rhuna.tech", // Substitua pelo seu endereço de e-mail
-//       pass: "Rhunatech2023#", // Substitua pela sua senha
-//     }
-//   });
+const transporter = nodemailer.createTransport({
+    host: "smtp.titan.email", // Substitua "yourdomain.com" pelo seu domínio na Hostinger
+    port: 465, // A porta pode ser 587 ou 465, dependendo das configurações do seu provedor
+    secure: true, // Use true para a porta 465, false para a porta 587
+    auth: {
+      user: "contato@rhuna.tech", // Substitua pelo seu endereço de e-mail
+      pass: "Rhunatech2023#", // Substitua pela sua senha
+    }
+  });
 
-// const mailOptions = {
-//     from: 'contato@rhuna.tech',
-//     to: 'destinatario <dest@gmail.com>',
-//     subject: 'salve',
-//     html: '<h5>salve</h5>'
-//   };
+const mailOptions = {
+    from: 'contato@rhuna.tech',
+    to: 'destinatario <dest@gmail.com>',
+    subject: 'salve',
+    html: '<h5>salve</h5>'
+  };
 
-// return transporter.sendMail(mailOptions, (erro, info) => {
-//     if (erro) {
-//       console.log('Erro ao enviar email');
-//       return reject(erro);
-//     }
-//     return resolve({ success: true });
-//   });
+return transporter.sendMail(mailOptions, (erro, info) => {
+    if (erro) {
+      console.log('Erro ao enviar email');
+      return reject(erro);
+    }
+    return resolve({ success: true });
+  });
 
 
 exports.HomeController = {
@@ -81,35 +80,7 @@ exports.HomeController = {
             // Senha incorreta
             return res.status(401).json({ mensagem: 'Credenciais inválidas' });
         }
-       
-
-        // try {
-        //     const registro = await RegistroController.getByEmail;
-            
-        //     let email = registro.params
-
-        //     if(!registro){
-        //         res.status(404).json({ mensagem: 'Email não encontrado, por favor registre-se.' });
-        //     }
-
-        //     if (registro) {
-        //         const senhaCorreta = await bcrypt.compare(senha, registro.senha);
-
-        //         if (senhaCorreta) {
-        //             req.usuarioAutenticado = registro;
-        //             next(); // Chama o próximo middleware ou rota
-        //         } else {
-        //             res.status(401).json({ mensagem: 'Credenciais inválidas' });
-        //         }
-        //     } else {
-        //         res.status(404).json({ mensagem: 'Usuário não encontrado' });
-        //     }
-        // } catch (error) {
-        //     console.error(error);
-        //     res.status(500).json({ mensagem: 'Erro durante a busca e comparação de senha' });
-        // }
     },
-
 }
 	
 
